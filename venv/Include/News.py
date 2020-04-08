b84793bd82ad81c17844aa98e9a61971c80833bb
chamith96 <chamithsandagiri@gmail.com>
2020-04-08 21:35:56 +0530

    updates

--- venv/Include/News.py
+++ venv/Include/News.py
@@ -2,38 +2,43 @@ import requests
 import json
 
 query = 'query'
-page_size = 'count'
-apiKey = 'apiKey'
+page_size = '200'
+apiKey = 'api key'
 from_date = '2020-01-01'
 to_date = '2020-01-31'
 
 url = "https://content.guardianapis.com/search?q=" + query + "&show-blocks=all&from-date=" + from_date + "&api-key=" + apiKey + "&page-size=" + page_size + "&to-date=" + to_date
 
 r = requests.get(url)
-results = r.json()['response']['results']
-
-f = open(query + ".json", "w")
-f.write('[')
 
 data = {}
 data['news'] = {}
 
-i = 0
-while i < int(page_size):
-    data['news'].update({
-        'date': r.json()['response']['results'][i]['webPublicationDate'],
-        'body': r.json()['response']['results'][i]['blocks']['body'][0]['bodyHtml']
-    })
+if int(page_size) <= 200:
+    f = open(query + "." + from_date + "." + to_date + ".json", "w")
+    f.write('[')
+    i = 0
+    while i < r.json()['response']['total']:
 
-    jsonVal = json.dumps(data['news'])
-    print(jsonVal)
+        data['news'].update({
+            'date': r.json()['response']['results'][i]['webPublicationDate'],
+            'body': r.json()['response']['results'][i]['blocks']['body'][0]['bodyHtml']
+        })
 
-    if int(page_size) - 1 == i:
-        f.write(jsonVal)
-        break
-    else:
-        f.write(jsonVal + ',')
-        i += 1
+        jsonVal = json.dumps(data['news'])
+        print(jsonVal)
 
-f.write(']')
-f.close()
+        if r.json()['response']['total'] - 1 == i:
+            f.write(jsonVal)
+            break
+        else:
+            f.write(jsonVal + ',')
+            i += 1
+
+    f.write(']')
+    f.close()
+    print("Total collected data: " + str(r.json()['response']['total']))
+
+
+else:
+    print("page count cannot more than 200")
